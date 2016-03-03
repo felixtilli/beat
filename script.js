4e5f9a01c1221523b35e52a293253854cf429514
unknown <rotab2@gmail.com>
2016-03-03 17:48:03 +0100

    Don't load audio file on every play

--- script.js
+++ script.js
@@ -2,30 +2,45 @@ var beats = [];
 var bpm = 0;
 var ms = 0;
 var hasBeenInitialized;
+var context = new AudioContext();
+
+function loadBeat() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "/beat.mp3", true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function() {
+        context.decodeAudioData(request.response, function(buffer) {
+            beatBuffer = buffer;
+        });
+    }
+    request.send();
+}
+loadBeat();
 
 var addBeat = function(){
     var previousBeat = beats.slice(-1)[0];
     var date = new Date();
-    
+
     var beat = {
-        date: date, 
+        date: date,
         diff: previousBeat ? (date - previousBeat.date) : 0
     }
-    
+
     beats.push(beat);
 }
 
 var setSpeed = function(){
     var total = 0;
-    
+
     if(beats.length > 3){
         beats = beats.slice(Math.max(beats.length - 3, 1));
     }
-    
+
     for(var i = 0; i < beats.length; i++){
         total += beats[i].diff;
     }
-    
+
     var calculatedBpm = Math.round(60000 * beats.length / total);
 
     if(isFinite(calculatedBpm) && calculatedBpm > 10){
@@ -37,8 +52,10 @@ var setSpeed = function(){
 
 var playBeat = function(){
     setTimeout(function(){
-        var beat = new Audio('/beat.mp3');
-        beat.play();
+        var source = context.createBufferSource();
+        source.buffer = beatBuffer;
+        source.connect(context.destination);
+        source.start(0);
         playBeat();
     }, ms);
 }
@@ -46,7 +63,7 @@ var playBeat = function(){
 var registerInput = function(){
     addBeat();
     setSpeed();
-    
+
     if(!hasBeenInitialized && ms !== 0){
         playBeat();
         hasBeenInitialized = true;
