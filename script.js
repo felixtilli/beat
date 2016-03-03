a1804a1893532545908cfc5cbc0ff5143e21ab46
Felix Tilli <felixtilli1@gmail.com>
2016-03-03 19:20:02 +0100

    Wrapped js code

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
-var beats = [];
-var bpm = 0;
-var ms = 0;
-var hasBeenInitialized;
-var context = new AudioContext();
+(function() {
+    var beats = [];
+    var bpm = 0;
+    var ms = 0;
+    var hasBeenInitialized;
+    var context = new AudioContext();
 
-function loadBeat() {
-    var request = new XMLHttpRequest();
-    request.open("GET", "/beat.mp3", true);
-    request.responseType = "arraybuffer";
+    function loadBeat() {
+        var request = new XMLHttpRequest();
+        request.open("GET", "/beat.mp3", true);
+        request.responseType = "arraybuffer";
 
-    request.onload = function() {
-        context.decodeAudioData(request.response, function(buffer) {
-            beatBuffer = buffer;
-        });
+        request.onload = function() {
+            context.decodeAudioData(request.response, function(buffer) {
+                beatBuffer = buffer;
+            });
+        }
+        request.send();
     }
-    request.send();
-}
-loadBeat();
+    loadBeat();
 
-var addBeat = function(){
-    var previousBeat = beats.slice(-1)[0];
-    var date = new Date();
-    
-    var beat = {
-        date: date, 
-        diff: previousBeat ? (date - previousBeat.date) : 0
+    var addBeat = function(){
+        var previousBeat = beats.slice(-1)[0];
+        var date = new Date();
+        
+        var beat = {
+            date: date, 
+            diff: previousBeat ? (date - previousBeat.date) : 0
+        }
+        
+        beats.push(beat);
     }
-    
-    beats.push(beat);
-}
 
-var setSpeed = function(){
-    var total = 0;
-    
-    if(beats.length > 3){
-        beats = beats.slice(Math.max(beats.length - 3, 1));
+    var setSpeed = function(){
+        var total = 0;
+        
+        if(beats.length > 3){
+            beats = beats.slice(Math.max(beats.length - 3, 1));
+        }
+        
+        for(var i = 0; i < beats.length; i++){
+            total += beats[i].diff;
+        }
+        
+        var calculatedBpm = Math.round(60000 * beats.length / total);
+
+        if(isFinite(calculatedBpm)){
+            ms = (total / beats.length);
+            bpm = calculatedBpm;
+            document.getElementById("bpm").innerHTML = bpm.toString() + " BPM";
+        }
     }
-    
-    for(var i = 0; i < beats.length; i++){
-        total += beats[i].diff;
+
+    var playBeat = function(){
+        setTimeout(function(){
+            var source = context.createBufferSource();
+            source.buffer = beatBuffer;
+            source.connect(context.destination);
+            source.start(0);
+            playBeat();
+        }, ms);
     }
-    
-    var calculatedBpm = Math.round(60000 * beats.length / total);
 
-    if(isFinite(calculatedBpm)){
-        ms = (total / beats.length);
-        bpm = calculatedBpm;
-        document.getElementById("bpm").innerHTML = bpm.toString() + " BPM";
+    var registerInput = function(){
+        addBeat();
+        setSpeed();
+        
+        if(!hasBeenInitialized && ms !== 0){
+            playBeat();
+            hasBeenInitialized = true;
+        }
     }
-}
 
-var playBeat = function(){
-    setTimeout(function(){
-        var source = context.createBufferSource();
-        source.buffer = beatBuffer;
-        source.connect(context.destination);
-        source.start(0);
-        playBeat();
-    }, ms);
-}
-
-var registerInput = function(){
-    addBeat();
-    setSpeed();
-    
-    if(!hasBeenInitialized && ms !== 0){
-        playBeat();
-        hasBeenInitialized = true;
-    }
-}
-
-window.addEventListener("keydown", registerInput, false);
+    window.addEventListener("keydown", registerInput, false);
+})();
